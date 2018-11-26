032d22509a96d97907704394e3d22fa767057ac5
MarkFrank01 <1123212438@qq.com>
2018-11-26 11:47:20 +0800

    add some description

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,10 @@ void main() {
 //  }
 //}
 
-//demo 2
+
+/**
+ * demo3 listview
+ */
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -49,17 +52,21 @@ class MyApp extends StatelessWidget {
     ];
 
     return MaterialApp(
-       title: 'Listview Demo',
-       home: Scaffold(
-         appBar: AppBar(
-           title: Text("Buildings"),
-         ),
-         body: BuildingListView(buildings, (index)=>debugPrint('SKTTT')),
-       ),
+      title: 'Listview Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Buildings"),
+        ),
+        body: BuildingListView(buildings, (index) => debugPrint('SKTTT')),
+      ),
     );
   }
+}
 
-//demo2
+/**
+ * demo2 布局
+ */
+//class MyApp extends StatelessWidget {
 //  @override
 //  Widget build(BuildContext context) {
 //    final titleSection = _TitleSection(
@@ -89,12 +96,17 @@ class MyApp extends StatelessWidget {
 //        appBar: AppBar(
 //          title: Text('Top Lakes1'),
 //        ),
+//        // 由于我们的内容可能会超出屏幕的长度，这里把内容都放到 ListView 里。
+//        // 除了这种用法，ListView 也可以像我们在 Android 原生开发中使用 ListView 那样，
+//        // 根据数据动态生成一个个 item。
 //        body: ListView(
 //          children: <Widget>[
 //            Image.asset(
 //              'images/image.jpg',
 //              width: 600,
 //              height: 240,
+//              // cover 类似于 Android 开发中的 centerCrop，其他一些类型，
+//              // https://docs.flutter.io/flutter/painting/BoxFit-class.html
 //              fit: BoxFit.cover,
 //            ),
 //            titleSection,
@@ -105,10 +117,11 @@ class MyApp extends StatelessWidget {
 //      ),
 //    );
 //  }
+//}
 
-}
-
-//demo3  ListView
+/**
+ * demo3 ListView
+ */
 enum BuildingType { theater, restaurant }
 
 class Building {
@@ -119,6 +132,7 @@ class Building {
   Building(this.type, this.title, this.address);
 }
 
+//定义一个回调接口
 typedef OnItemClickListener = void Function(int position);
 
 class ItemView extends StatelessWidget {
@@ -126,6 +140,7 @@ class ItemView extends StatelessWidget {
   final Building building;
   final OnItemClickListener listener;
 
+  //这里的listener会从ListView那传递过来
   ItemView(this.position, this.building, this.listener);
 
   @override
@@ -161,9 +176,11 @@ class ItemView extends StatelessWidget {
       ],
     );
 
+    // 一般来说，为了监听手势事件，我们使用 GestureDetector。但这里为了在点击的时候有个
+    // 水波纹效果，使用的是 InkWell。
     return InkWell(
-      onTap: ()=>listener(position),
-        child: widget,
+      onTap: () => listener(position),
+      child: widget,
     );
   }
 }
@@ -172,6 +189,7 @@ class BuildingListView extends StatelessWidget {
   final List<Building> buildings;
   final OnItemClickListener listener;
 
+  //这是对外接口。外部通过构造函数闯入数据和listener
   BuildingListView(this.buildings, this.listener);
 
   @override
@@ -195,14 +213,22 @@ class _TitleSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    //为了加上padding，嵌套一个Container
     return Container(
+      //设置padding的距离
       padding: EdgeInsets.all(32.0),
       child: Row(
         children: <Widget>[
+          //嵌套为了让标题占满剩余的空间
           Expanded(
+            //Expanded只能包含一个子元素,使用的是child
             child: Column(
+              // Column 是竖直方向的，cross 为交叉的意思，也就是说，这里设置的是水平方向
+              // 的对齐。在水平方向，我们让文本对齐到 start
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
+                //同之前的，为了加入padding
                 Container(
                   padding: const EdgeInsets.only(bottom: 8.0),
                   child: Text(
@@ -217,6 +243,7 @@ class _TitleSection extends StatelessWidget {
               ],
             ),
           ),
+          //第二个元素
           Icon(
             Icons.star,
             color: Colors.red[500],
@@ -233,7 +260,14 @@ Widget _buildButtonColum(BuildContext context, IconData icon, String label) {
   final color = Theme.of(context).primaryColor;
 
   return Column(
+    // main axis 跟我们前面提到的 cross axis 相对应，对 Column 来说，指的就是竖直方向。
+    // 在放置完子控件后，屏幕上可能还会有一些剩余的空间（free space），min 表示尽量少占用
+    // free space；类似于 Android 的 wrap_content。
+    // 对应的，还有 MainAxisSize.max
+
     mainAxisSize: MainAxisSize.min,
+
+    // 沿着 main axis 居中放置
     mainAxisAlignment: MainAxisAlignment.center,
     children: <Widget>[
       Icon(icon, color: color),
@@ -253,97 +287,97 @@ Widget _buildButtonColum(BuildContext context, IconData icon, String label) {
 }
 
 //demo1
-//class RandomWords extends StatefulWidget {
-//  @override
-//  createState() => new RandomWordsState();
-//}
-//
-//class RandomWordsState extends State<RandomWords> {
-//  final _suggestions = <WordPair>[];
-//
-//  final _biggerFont = const TextStyle(fontSize: 18.0);
-//
-//  final _saved = new Set<WordPair>();
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return new Scaffold(
-//      appBar: new AppBar(
-//        title: new Text("Startup Name"),
-//        actions: <Widget>[
-//          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
-//        ],
-//      ),
-//      body: _buildSuggestions(),
-//    );
-//  }
-//
-//  void _pushSaved() {
-//    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
-//      final titles = _saved.map((pair) {
-//        return new ListTile(
-//          title: new Text(
-//            pair.asPascalCase,
-//            style: _biggerFont,
-//          ),
-//        );
-//      });
-//
-//      final divided = ListTile.divideTiles(
-//        tiles: titles,
-//        context: context,
-//      ).toList();
-//
-//      return new Scaffold(
-//        appBar: new AppBar(
-//          title: new Text("Save Suggestions"),
-//        ),
-//        body: new ListView(children: divided),
-//      );
-//    }));
-//  }
-//
-//  Widget _buildSuggestions() {
-//    return new ListView.builder(
-//        padding: const EdgeInsets.all(16.0),
-//        //每次生成一个单词时被回调
-//
-//        itemBuilder: (context, i) {
-//          if (i.isOdd) return new Divider();
-//
-//          final index = i ~/ 2;
-//
-//          if (index >= _suggestions.length) {
-//            _suggestions.addAll(generateWordPairs().take(10));
-//          }
-//
-//          return _buildRow(_suggestions[index]);
-//        });
-//  }
-//
-//  Widget _buildRow(WordPair pair) {
-//    final alreadySaved = _saved.contains(pair);
-//
-//    return new ListTile(
-//      title: new Text(
-//        pair.asPascalCase,
-//        style: _biggerFont,
-//      ),
-//      //加入心型图标
-//      trailing: new Icon(
-//        alreadySaved ? Icons.favorite : Icons.favorite_border,
-//        color: alreadySaved ? Colors.red : null,
-//      ),
-//      onTap: () {
-//        //在Flutter的响应式架构中,调用setState()会触发对 State 对象的 build() 方法的调用，从而导致UI的更新。
-//        setState(() {
-//          if (alreadySaved) {
-//            _saved.remove(pair);
-//          } else {
-//            _saved.add(pair);
-//          }
-//        });
-//      },
-//    );
-//  }
-//}
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  final _saved = new Set<WordPair>();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Startup Name"),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      final titles = _saved.map((pair) {
+        return new ListTile(
+          title: new Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      });
+
+      final divided = ListTile.divideTiles(
+        tiles: titles,
+        context: context,
+      ).toList();
+
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Save Suggestions"),
+        ),
+        body: new ListView(children: divided),
+      );
+    }));
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        //每次生成一个单词时被回调
+
+        itemBuilder: (context, i) {
+          if (i.isOdd) return new Divider();
+
+          final index = i ~/ 2;
+
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      //加入心型图标
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        //在Flutter的响应式架构中,调用setState()会触发对 State 对象的 build() 方法的调用，从而导致UI的更新。
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+}
